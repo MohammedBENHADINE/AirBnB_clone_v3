8fbd52cedb38caa434da625d7860847bdce41501
Mohammed BENHADINE <mohammedbenhadine@gmail.com>
2023-10-31 02:53:09 -0700

    task 3

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
-"""AirBnB v3 flask Api v1 entrypoint"""
-import flask
+
+from api.v1.views import app_views
+
+@app_views.route('/status')
+def status():
+    return {"status": "OK"}
